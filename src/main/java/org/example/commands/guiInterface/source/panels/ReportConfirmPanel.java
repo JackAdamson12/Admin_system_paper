c541f486a72a39b6596d8ee0db1aef749382ba03
Ivan <djekadamson123321@gmail.com>
2026-08-30 17:18:34 +0200

    Created report system,also created ticked system for moderation

---
+++ src/main/java/org/example/commands/guiInterface/source/panels/ReportConfirmPanel.java
@@ -0,0 +1,27 @@
+package org.example.commands.guiInterface.source.panels;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.example.commands.guiInterface.source.utils.ButtomCreator;
+import org.example.commands.guiInterface.source.utils.GuiTitles;
+
+public class ReportConfirmPanel
+{
+    private final ButtomCreator buttonCreator;
+
+    public ReportConfirmPanel(ButtomCreator buttonCreator)
+    {
+        this.buttonCreator = buttonCreator;
+    }
+
+    public void open(Player player)
+    {
+        Inventory inventory = Bukkit.createInventory(null, 27, GuiTitles.REPORT_CONFIRM_PANEL);
+        inventory.setItem(11, buttonCreator.createButton(Material.LIME_WOOL, ChatColor.GREEN + "Take case"));
+        inventory.setItem(15, buttonCreator.createButton(Material.RED_WOOL, ChatColor.RED + "Cancel"));
+        player.openInventory(inventory);
+    }
+}
